dbd471b884ee1f09d44a1c19ad6ccb0bcdfccc90
bedouioussama <bedouioussama@gmail.com>
2026-03-28 15:23:19 +0000

    working on sanity

---
+++ dist/static/sanity-DvTl_TYI.js
@@ -0,0 +1,1 @@
+import{r as t,s as e}from"./sanity.config--EA9-T-t.js";t(document.getElementById("sanity"),e,{reactStrictMode:!1,basePath:"/"});
---
+++ src/sanity/schema.ts
@@ -0,0 +1,4 @@
+import blog from './schemaTypes/blog';
+import news from './schemaTypes/news';
+
+export const schemaTypes = [blog, news];
